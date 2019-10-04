887686fb6d1a78606fea8a8fa0846aca5d015ef9
Michael Wolff <mlwolff@gmx.net>
2019-10-04 11:08:43 +0200

    PrimApp added.

---
+++ misc/src/main/java/com/lupus/main/PrimApp.java
@@ -0,0 +1,13 @@
+package com.lupus.main;
+
+import com.lupus.utils.Factorizer;
+
+public class PrimApp {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println(Factorizer.factorize(45650521487954L));
+        
+    }
+
+}
---
+++ misc/src/main/java/com/lupus/utils/Factorizer.java
@@ -0,0 +1,46 @@
+package com.lupus.utils;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Factorizer {
+    
+    /**
+     * Private constructor.
+     */
+    private Factorizer() {};
+    
+    /**
+     * 
+     * @param number
+     * @return
+     */
+    public static Map<Long, Long> factorize(long number) {
+        Map<Long, Long> rc = new LinkedHashMap<Long, Long>();
+        long remaingFactor = number;
+        long currentFactor = 2;
+        long currentCount = 0;
+        
+        long start = System.currentTimeMillis();
+        
+        while(remaingFactor > 1) {
+            while (remaingFactor % currentFactor == 0 && remaingFactor > 1) {
+                remaingFactor /= currentFactor;
+                currentCount++;
+            }
+            
+            if (currentCount > 0) {
+                rc.put(currentFactor, currentCount);
+            }
+            
+            currentCount = 0;
+            currentFactor += 1+currentFactor % 2;
+        }
+        
+        
+        System.out.println(String.format("factorize() [%dms]", System.currentTimeMillis() - start));
+        
+        return rc;
+    }
+}
